ea8308f324c023f4e279fc55f58a4840918d0c13
Lucjan Wiśnicz <luqapo@gmail.com>
2018-10-27 11:44:30 +0200

    host

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.static('./public/'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(5000,  "0.0.0.0", () => {
     console.log(`Server listen at port ${PORT}`);
 })
