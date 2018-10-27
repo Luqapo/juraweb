3f38f0d6a458e9743ceb040384204ceeb11e7d39
Lucjan Wiśnicz <luqapo@gmail.com>
2018-10-27 11:39:13 +0200

    port cd

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public/'));
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(5000, () => {
-    console.log('Server listen at port 5000');
+    console.log(`Server listen at port ${PORT}`);
 })
