cb8233c581963cf6ae59d22d4ac7748724c22ca9
Lucjan Wiśnicz <luqapo@gmail.com>
2018-10-27 15:41:52 +0200

    join

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
