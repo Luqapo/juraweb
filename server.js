2eff9673a1bc9a04a30ac3570b72b7409d08dc09
Lucjan Wiśnicz <luqapo@gmail.com>
2018-10-29 22:07:57 +0100

    add body parser to router

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', AuthController);
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 router.use((req,res,next) => {
     console.log('Connection');
