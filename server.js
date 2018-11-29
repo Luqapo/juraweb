9c0cdbd2c09ea0754f35fe450d8654368e5dba8d
Lucjan Wiśnicz <luqapo@gmail.com>
2018-11-29 19:58:12 +0100

    server change for routing

--- server.js
+++ server.js
@@ -17,16 +17,18 @@ mongoose.connect(uri, {useNewUrlParser: true}, () => {
 const app = express();
 const PORT = process.env.PORT || 5000;
 const router = express.Router();
+app.use((req,res,next) => {
+    const url = req.url;
+    if(url === '/east' || url === '/center' || url === '/north'){
+        res.sendFile(path.join(__dirname+'/public/index.html'));
+    }
+    next();
+});
 app.use('/api', router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', AuthController);
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-
-router.use((req,res,next) => {
-    console.log('Connection');
-    next();
-});
 
 router.route('/regiony/:region_id')
     .get((req,res) => {
@@ -124,10 +126,6 @@ router.route('/ascents/add')
                 .catch(err => console.log(err));
                 res.send({ message:'Ascent added' })
          });
- 
-// app.get('*', (req,res) => {
-//     res.sendFile(path.join(__dirname+'/public/index.html'))
-// })
 
 app.listen(PORT,  "0.0.0.0", () => {
     console.log(`Server listen at port ${PORT}`);
